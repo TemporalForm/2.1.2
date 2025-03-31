f0b8fd9f2b79a87c65f380ca44be69107c697dea
Max Sheremet <leheupofall@yandex.ru>
2025-03-31 20:39:58 +0300

    Create beans with different Scope types and compare them

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,11 +4,20 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(value = "cat")
+    @Scope(value = "prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setAge(2);
+        cat.setName("Cat");
+        return cat;
     }
 }
